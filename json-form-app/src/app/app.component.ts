8af616d391c2e3c66464dd898d0fa292069a318d
Diego Caceres <diegodanielcaceres10@gmail.com>
2022-08-09 14:44:39 -0300

    Fix min and max lent help and error

--- json-form-app/src/app/app.component.ts
+++ json-form-app/src/app/app.component.ts
@@ -15,137 +15,10 @@ export class AppComponent {
             {
                 fields: [
                     {
-                        name: "hiddenname",
-                        type: "hidden",
-                        value: 1,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "textname",
-                        type: "text",
-                        label: "Text",
-                        value: "Example Text",
-                        required: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "emailname",
-                        type: "email",
-                        label: "E-Mail",
-                        value: "exampleemail@mail.com",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "telname",
-                        type: "tel",
-                        label: "Tel",
-                        value: "(12) 34567-8912",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "text2name",
-                        type: "text",
-                        label: "Text Inline Left",
-                        required: true,
-                    },
-                    {
-                        name: "text3name",
-                        type: "text",
-                        label: "Text Inline center",
-                    },
-                    {
-                        name: "text4name",
-                        type: "text",
-                        label: "Text Inline Right",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "textdisabledname",
-                        type: "text",
-                        label: "Text Disabled",
-                        disabled: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "passwordname",
+                        name: "password",
                         type: "password",
-                        label: "Password",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "numbername",
-                        type: "number",
-                        label: "Number",
-                        value: 100,
-                    },
-                    {
-                        name: "numberminname",
-                        type: "number",
-                        label: "Number Min Value 50",
-                        min: 50,
-                    },
-                    {
-                        name: "numbermaxname",
-                        type: "number",
-                        label: "Number Max Value 50",
-                        max: 50,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "checkboxname",
-                        type: "checkbox",
-                        label: "CheckBox",
-                        text: "CheckBox",
-                    },
-                    {
-                        name: "checkbox2name",
-                        type: "checkbox",
-                        text: "CheckBox2",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "radioname",
-                        type: "radio",
-                        label: "Radio",
-                        options: ["Option 1", "Option 2", "Option 3"],
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "selectname",
-                        type: "select",
-                        label: "Element Select",
-                        options: ["Option 1", "Option 2", "Option 3"],
-                        searchable: true,
+                        label: "DATA.PASSWORD",
+                        minlength: 8,
                         required: true,
                     },
                 ],
@@ -153,102 +26,30 @@ export class AppComponent {
             {
                 fields: [
                     {
-                        name: "selectdisabledname",
-                        type: "select",
-                        label: "Element Select Disabled",
-                        options: ["Option 1", "Option 2", "Option 3"],
-                        searchable: true,
+                        name: "confirmpassword",
+                        type: "password",
+                        label: "DATA.CONFIRMPASSWORD",
                         required: true,
-                        disabled: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "selectmultiplename",
-                        type: "select",
-                        label: "Multiple Element Select",
-                        options: ["Option 1", "Option 2", "Option 3"],
-                        multiple: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "listname",
-                        type: "list",
-                        label: "Element List",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "colorname",
-                        type: "color",
-                        label: "Color",
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "filename",
-                        type: "file",
-                        label: "File upload.",
-                        required: true,
-                    },
-                    {
-                        name: "filemultiplename",
-                        type: "file",
-                        label: "Multiple File upload.",
-                        multiple: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "imagename",
-                        type: "image",
-                        label: "Image logo. 100 KB Max Size",
-                        maxsize: 100000,
-                    },
-                    {
-                        name: "imagecovername",
-                        type: "image",
-                        label: "Image photo",
-                        cover: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "imagemultiplename",
-                        type: "image",
-                        label: "Images",
-                        multiple: true,
-                    },
-                ],
-            },
-            {
-                fields: [
-                    {
-                        name: "textareaname",
-                        type: "textarea",
-                        label: "Textarea",
-                        required: true,
+                        actions: [
+                            {
+                                type: "match",
+                                field: "password",
+                            },
+                        ],
                     },
                 ],
             },
         ],
         buttons: [
             {
-                text: "Submit button",
+                text: "No",
+                event: "no",
+            },
+            {
+                text: "Yes",
                 submit: true,
+                primary: true,
+                spinner: true,
             },
         ],
         format: {
@@ -277,18 +78,6 @@ export class AppComponent {
                         required: true,
                     },
                 ],
-            },
-        ],
-        buttons: [
-            {
-                text: "No",
-                event: "no",
-            },
-            {
-                text: "Yes",
-                submit: true,
-                primary: true,
-                spinner: true,
             },
         ],
         format: {
