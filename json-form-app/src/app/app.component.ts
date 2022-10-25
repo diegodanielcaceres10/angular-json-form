ff99e98d5f7c362cc61b5008f22d66fea5be60fa
Diego Caceres <diegodanielcaceres10@gmail.com>
2022-10-24 22:20:50 -0300

    Add input type date

--- json-form-app/src/app/app.component.ts
+++ json-form-app/src/app/app.component.ts
@@ -8,9 +8,111 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'json-form-app';
     form: any = {
+        lang: "pt-BR",
         title: "Title From Header",
         text: "Example text description. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dicta dolor ipsam iure. Corporis, tenetur eaque commodi deserunt modi dignissimos nostrum rem velit nihil accusamus debitis, reiciendis aperiam repudiandae asperiores.",
         groups: [
+            {
+                fields: [
+                    {
+                        name: "hiddenname",
+                        type: "hidden",
+                        value: 1,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "textname",
+                        type: "text",
+                        label: "Text",
+                        value: "Example Text",
+                        required: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "emailname",
+                        type: "email",
+                        label: "E-Mail",
+                        value: "exampleemail@mail.com",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "telname",
+                        type: "tel",
+                        label: "Tel",
+                        value: "(12) 34567-8912",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "text2name",
+                        type: "text",
+                        label: "Text Inline Left",
+                        required: true,
+                    },
+                    {
+                        name: "text3name",
+                        type: "text",
+                        label: "Text Inline center",
+                    },
+                    {
+                        name: "text4name",
+                        type: "text",
+                        label: "Text Inline Right",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "textdisabledname",
+                        type: "text",
+                        label: "Text Disabled",
+                        disabled: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "passwordname",
+                        type: "password",
+                        label: "Password",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "numbername",
+                        type: "number",
+                        label: "Number",
+                        value: 100,
+                    },
+                    {
+                        name: "numberminname",
+                        type: "number",
+                        label: "Number Min Value 50",
+                        min: 50,
+                    },
+                    {
+                        name: "numbermaxname",
+                        type: "number",
+                        label: "Number Max Value 50",
+                        max: 50,
+                    },
+                ],
+            },
             {
                 fields: [
                     {
@@ -23,10 +125,141 @@ export class AppComponent {
             {
                 fields: [
                     {
-                        name: "datename2",
+                        name: "daterangename",
                         type: "date",
-                        label: "Date Range",
+                        label: "Date",
                         range: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "checkboxname",
+                        type: "checkbox",
+                        label: "CheckBox",
+                        text: "CheckBox",
+                    },
+                    {
+                        name: "checkbox2name",
+                        type: "checkbox",
+                        text: "CheckBox2",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "radioname",
+                        type: "radio",
+                        label: "Radio",
+                        options: ["Option 1", "Option 2", "Option 3"],
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "selectname",
+                        type: "select",
+                        label: "Element Select",
+                        options: ["Option 1", "Option 2", "Option 3"],
+                        searchable: true,
+                        required: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "selectdisabledname",
+                        type: "select",
+                        label: "Element Select Disabled",
+                        options: ["Option 1", "Option 2", "Option 3"],
+                        searchable: true,
+                        required: true,
+                        disabled: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "selectmultiplename",
+                        type: "select",
+                        label: "Multiple Element Select",
+                        options: ["Option 1", "Option 2", "Option 3"],
+                        multiple: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "listname",
+                        type: "list",
+                        label: "Element List",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "colorname",
+                        type: "color",
+                        label: "Color",
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "filename",
+                        type: "file",
+                        label: "File upload.",
+                        required: true,
+                    },
+                    {
+                        name: "filemultiplename",
+                        type: "file",
+                        label: "Multiple File upload.",
+                        multiple: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "imagename",
+                        type: "image",
+                        label: "Image logo. 100 KB Max Size",
+                        maxsize: 100000,
+                    },
+                    {
+                        name: "imagecovername",
+                        type: "image",
+                        label: "Image photo",
+                        cover: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "imagemultiplename",
+                        type: "image",
+                        label: "Images",
+                        multiple: true,
+                    },
+                ],
+            },
+            {
+                fields: [
+                    {
+                        name: "textareaname",
+                        type: "textarea",
+                        label: "Textarea",
+                        required: true,
                     },
                 ],
             },
@@ -37,6 +270,9 @@ export class AppComponent {
                 submit: true,
             },
         ],
+        format: {
+            primary: "lime",
+        },
     };
     form2: any = {
         title: "Example formated and full size.",
